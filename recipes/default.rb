b447b7bea843351ddf2ce8f64ae067e56edcee52
Daniel Flynn <dflynn@spartaglobal.com>
2019-08-16 13:18:21 +0100

    Created install python in recipe, created simple test in unittest

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,11 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+
+apt_update 'update_sources' do
+  action :update
+end
+
+package 'python3' do
+  action :install
+end
